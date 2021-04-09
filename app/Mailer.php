9640e2634b13a0aa24fbee30f9158014a8c60eb3
Austin Ginder <austinginder@gmail.com>
2021-04-09 14:20:36 -0400

    👌 IMPROVE: Email invites

--- app/Mailer.php
+++ app/Mailer.php
@@ -10,9 +10,12 @@ class Mailer {
         if ( ! function_exists( 'get_home_path' ) ) {
             include_once ABSPATH . '/wp-admin/includes/file.php';
         }
-        $event     = ( new Event( $event_id ) )->fetch();
-        $home_path = get_home_path();
-        $path      = "{$home_path}invites/{$event->event_id}/";
+        $event      = ( new Event( $event_id ) )->fetch();
+        $group      = ( new Group( $event->group_id ) );
+        $members    = $group->members();
+        $group_data = $group->fetch();
+        $home_path  = get_home_path();
+        $path       = "{$home_path}invites/{$event->event_id}/";
         if ( ! is_dir( $path ) ) {
             mkdir ( $path );
         }
@@ -22,26 +25,27 @@ class Mailer {
 
         $generated_ics = SpatieCalendar::create( $event->name )
             ->event( SpatieEvent::create( $event->name )
+                ->organizer( $group_data->reply_to_email, $group_data->reply_to_name )
                 ->startsAt( new \DateTime( $event->event_at, new \DateTimeZone('America/New_York') ) )
                 ->endsAt( $ends_at )
             )
             ->get();
 
         file_put_contents( "{$path}invite.ics", $generated_ics );
-        $group       = ( new Group( $event->group_id ) );
-        $members     = $group->members();
-        $group_data  = $group->fetch();
-        $event_at    = date('D M jS Y \a\t h:ia', strtotime( $event->event_at ) );
-        $subject     = "{$event->name} at $event_at";
+
+        $event_at    = date('F jS', strtotime( $event->event_at ) );
+        $subject     = "{$event->name} on $event_at";
         $attachments = [ "{$path}invite.ics" ];
         $reply_to    = "Reply-To: {$group_data->reply_to_name} <{$group_data->reply_to_email}>";
         $headers     = [ 'Content-Type: text/html; charset=UTF-8', $reply_to ];
+        $event_link  = home_url() . "/group/{$group->fetch()->slug}/{$event->slug}";
+        $rsvp        = '<hr><a href="'.$event_link.'" style="padding: 12px 32px; font-size: 18px;background-color: #eee!important;border-color: #eee!important;margin: 10px;display: inline-block;color: black;text-decoration: none;font-weight: bold;">RSVP TO ATTEND EVENT</a>';
 
         foreach( $members as $member ) {
             $token         = $member->member_id . "-" . md5( $member->created_at );
             $leave_link    = home_url() . "/group/{$group->fetch()->slug}/leave?token=$token";
             $member_footer = str_replace( "[leave_group]", $leave_link, $group_data->email_footer );
-            $body          = "{$member->first_name}, <br />$event->description $member_footer";
+            $body          = "{$member->first_name}, <br />$event->description $rsvp $member_footer";
             wp_mail( $member->email, $subject, $body, $headers, $attachments );
         }
 
